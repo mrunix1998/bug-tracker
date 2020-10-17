d3e8014854254615f26d9e01cb53ebe6f4e719e8
Amirhossein Saemi <saemiamirhossein@gmail.com>
2020-10-17 14:41:55 +0330

    added email sender

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,12 @@
+import logging
+import datetime
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from smtplib import SMTPException
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 class Bug(models.Model):
@@ -13,3 +20,25 @@ class Bug(models.Model):
 
     def __str__(self):
         return self.title
+
+    def send_email(self):
+        # do logging stuff here
+        if self.assigned_to:
+            logger.info(f"[{datetime.datetime.now()}]: Started Sending Bug-{self.pk} to {self.assigned_to}")
+            try:
+                send_mail(
+                    f'[BUG ALERT] {self.title}',
+                    self.description,
+                    'bug@bug-tracker.com',
+                    [self.assigned_to.email],
+                )
+                self.is_notified = True
+                logger.info(f"[{datetime.datetime.now()}]: Finished Sending Bug-{self.pk} to {self.assigned_to}")
+
+            except (SMTPException, OSError):
+                self.is_notified = False
+                logger.error(f"[{datetime.datetime.now()}]: Failed Sending Bug-{self.pk} to {self.assigned_to}")
+
+
+
+
