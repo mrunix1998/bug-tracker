a235ef867903e75313e88e450455dc850f533443
Amirhossein Saemi <saemiamirhossein@gmail.com>
2020-10-22 10:04:22 +0330

    updated gmail

--- bug_tracker/settings.py
+++ bug_tracker/settings.py
@@ -129,3 +129,14 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/home/user/static/'
 
 MEDIA_ROOT = '/home/user/media/'
+
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = 'bugs.manyx.tracker@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'manyxmanyx'
